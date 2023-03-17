a865338e57c8f11a2ea2d2556b23cc47989397e6
VictoriaObasa <124203907+Victoria-Obasa@users.noreply.github.com>
2023-03-17 11:32:30 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -50,6 +50,54 @@ def create_order():
     )
 
 
+# RETRIEVE AN ACCOUNT
+
+@app.route("/orders/<int:order_id>", methods=["GET"])
+def get_orders(order_id):
+    """
+    Retrieve a single Order
+    This endpoint will return an Order based on it's id
+    """
+    app.logger.info("Request for Order with id: %s", order_id)
+
+    # See if the order exists and abort if it doesn't
+    order = Order.find(order_id)
+    if not order:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Order with id '{order_id}' could not be found.",
+        )
+
+    return make_response(jsonify(order.serialize()), status.HTTP_200_OK)
+
+
+
+# UPDATE AN EXISTING ACCOUNT
+
+@app.route("/orders/<int:order_id>", methods=["PUT"])
+def update_orders(order_id):
+    """
+    Update an Order
+    This endpoint will update an Order based the body that is posted
+    """
+    app.logger.info("Request to update order with id: %s", order_id)
+    check_content_type("application/json")
+
+    # See if the order exists and abort if it doesn't
+    order = Order.find(order_id)
+    if not order:
+        abort(
+            status.HTTP_404_NOT_FOUND, f"Order with id '{order_id}' was not found."
+        )
+
+    # Update from the json in the body of the request
+    order.deserialize(request.get_json())
+    order.id = order_id
+    order.update()
+
+    return make_response(jsonify(account.serialize()), status.HTTP_200_OK)
+
+
 
 # DELETE AN ORDER
 
@@ -72,6 +120,115 @@ def delete_orders(order_id):
 # ---------------------------------------------------------------------
 #                ITEMS   M E T H O D S
 # ---------------------------------------------------------------------
+
+
+# CREATE/ADD AN ADDRESS TO AN ACCOUNT
+
+@app.route("/orders/<int:order_id>/items", methods=["POST"])
+def create_items(order_id):
+    """
+    Create an Item on an Order
+    This endpoint will add an item to an order
+    """
+    app.logger.info("Request to create an Item for Order with id: %s", order_id)
+    check_content_type("application/json")
+
+    # See if the order exists and abort if it doesn't
+    order = Order.find(order_id)
+    if not order:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Order with id '{order_id}' could not be found.",
+        )
+
+    # Create an item from the json data
+    iem = Item()
+    item.deserialize(request.get_json())
+
+    # Append the item to the order
+    order.items.append(item)
+    order.update()
+
+    # Prepare a message to return
+    message = item.serialize()
+
+    return make_response(jsonify(message), status.HTTP_201_CREATED)
+
+
+# LIST ITEMS
+
+@app.route("/orders/<int:order_id>/items", methods=["GET"])
+def list_items(account_id):
+    """Returns all of the Items for an Order"""
+    app.logger.info("Request for all Items for an Order with id: %s", order_id)
+
+    # See if the order exists and abort if it doesn't
+    order = Order.find(order_id)
+    if not order:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Order with id '{order_id}' could not be found.",
+        )
+
+    # Get the items for the account
+    results = [item.serialize() for item in order.items]
+
+    return make_response(jsonify(results), status.HTTP_200_OK)
+
+
+######################################################################
+# RETRIEVE AN ITEM FROM ORDER
+######################################################################
+@app.route("/orders/<int:order_id>/items/<int:item_id>", methods=["GET"])
+def get_items(order_id, item_id):
+    """
+    Get an Item
+    This endpoint returns just an item
+    """
+    app.logger.info(
+        "Request to retrieve Item %s for Order id: %s", (item_id, order_id)
+    )
+
+    # See if the item exists and abort if it doesn't
+    item = Item.find(item_id)
+    if not item:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Order with id '{order_id}' could not be found.",
+        )
+
+    return make_response(jsonify(address.serialize()), status.HTTP_200_OK)
+
+
+######################################################################
+# UPDATE AN ITEM
+######################################################################
+@app.route("/orders/<int:order_id>/items/<int:item_id>", methods=["PUT"])
+def update_items(order_id, item_id):
+    """
+    Update an Item
+    This endpoint will update an Item based the body that is posted
+    """
+    app.logger.info(
+        "Request to update Item %s for Order id: %s", (item_id, order_id)
+    )
+    check_content_type("application/json")
+
+    # See if the item exists and abort if it doesn't
+    item = Item.find(item_id)
+    if not item:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Order with id '{item_id}' could not be found.",
+        )
+
+    # Update from the json in the body of the request
+    item.deserialize(request.get_json())
+    item.id = item_id
+    item.update()
+
+    return make_response(jsonify(address.serialize()), status.HTTP_200_OK)
+
 
 
 ######################################################################
