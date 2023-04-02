ea7ae405ca48841ae27760a2ffbe8bd876fe66bb
Bart Consedine <bartconsedine@gmail.com>
2023-04-02 19:14:51 -0400

    added in the current_app stuff

--- service/routes.py
+++ service/routes.py
@@ -4,7 +4,7 @@ My Service
 Describe what your service does here
 """
 
-from flask import url_for, jsonify, request, make_response, abort, Blueprint
+from flask import url_for, jsonify, request, make_response, abort, Blueprint, current_app
 from service.common import status  # HTTP Status Codes
 from service.models import Order, Item
 
@@ -42,7 +42,7 @@ def create_order():
     Creates an Order
     This endpoint will create an Order based the data in the body that is posted
     """
-    app.logger.info("Request to create an Order")
+    current_app.logger.info("Request to create an Order")
     check_content_type("application/json")
 
     # Create the order
@@ -70,7 +70,7 @@ def get_orders(order_id):
     Retrieve a single Order
     This endpoint will return an Order based on its id
     """
-    app.logger.info("Request for Order with id: %s", order_id)
+    current_app.logger.info("Request for Order with id: %s", order_id)
 
     # See if the order exists and abort if it doesn't
     order = Order.find(order_id)
@@ -91,7 +91,7 @@ def get_orders(order_id):
 @routes_bp.route("/orders", methods=["GET"])
 def list_orders():
     """Returns all of the Orders"""
-    app.logger.info("Request for Order list")
+    current_app.logger.info("Request for Order list")
     orders = []
 
     # Process the query string if any
@@ -118,7 +118,7 @@ def update_orders(order_id):
     Update an Order
     This endpoint will update an Order based the body that is posted
     """
-    app.logger.info("Request to update order with id: %s", order_id)
+    current_app.logger.info("Request to update order with id: %s", order_id)
     check_content_type("application/json")
 
     # See if the order exists and abort if it doesn't
@@ -146,7 +146,7 @@ def delete_orders(order_id):
     Delete an Order
     This endpoint will delete an order based the id specified in the path
     """
-    app.logger.info("Request to delete an order with id: %s", order_id)
+    current_app.logger.info("Request to delete an order with id: %s", order_id)
 
     # Retrieve the order to delete and delete it if it exists
     order = Order.find(order_id)
@@ -192,7 +192,7 @@ def create_items(order_id):
     Create an Item on an Order
     This endpoint will add an item to an order
     """
-    app.logger.info(
+    current_app.logger.info(
         "Request to create an Item for Order with id: %s", order_id)
     check_content_type("application/json")
 
@@ -226,7 +226,7 @@ def create_items(order_id):
 @routes_bp.route("/orders/<int:order_id>/items", methods=["GET"])
 def list_items(order_id):
     """Returns all of the Items for an Order"""
-    app.logger.info("Request for all Items for an Order with id: %s", order_id)
+    current_app.logger.info("Request for all Items for an Order with id: %s", order_id)
 
     # See if the order exists and abort if it doesn't
     order = Order.find(order_id)
@@ -253,7 +253,7 @@ def get_items(order_id, item_id):
     Get an Address
     This endpoint returns just an address
     """
-    app.logger.info(
+    current_app.logger.info(
         "Request to retrieve Address %s for Account id: %s", (
             item_id, order_id)
     )
@@ -280,7 +280,7 @@ def update_items(order_id, item_id):
     Update an Item
     This endpoint will update an Item based the body that is posted
     """
-    app.logger.info("Request to update Item %s for Order id: %s",
+    current_app.logger.info("Request to update Item %s for Order id: %s",
                     (item_id, order_id))
     check_content_type("application/json")
 
@@ -311,7 +311,7 @@ def delete_items(order_id, item_id):
     Delete an Order Item
     This endpoint will delete an item based the id specified in the path
     """
-    app.logger.info("Request to delete item %s for order id: %s",
+    current_app.logger.info("Request to delete item %s for order id: %s",
                     (order_id, item_id))
 
     # See if the item exists and delete it if it does
@@ -332,7 +332,7 @@ def check_content_type(media_type):
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
         return
-    app.logger.error("Invalid Content-Type: %s", content_type)
+    current_app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
